d115d1624ddf87c014b6d2cdba5c506784ff4246
molpopgen <krthornt@uci.edu>
2022-03-07 15:53:58 -0800

    more figs

---
+++ genetics/figures/loss_of_genetic_material.R
@@ -0,0 +1,29 @@
+library(pedtools)
+library(ibdsim2)
+
+png("loss_of_genetic_material.png", height = 800, width = 800)
+
+chr19 <- loadMap("decode19", chrom = 1)
+
+
+ngens <- 3
+
+p <- ancestralPed(ngens)
+
+sim <- ibdsim(p, N = 1, map = chr19, seed = 1234, verbose = F, model = "haldane")
+
+nanc <- 2**ngens
+
+# NOTE: the next bits are a little messy
+
+pos <- rep(1, (2**ngens))
+
+pos <- c(pos, rep(c(2, 4), sum(2**((ngens - 1) - 1:(ngens)))))
+
+pos <- c(pos, 2)
+
+haploDraw(p, sim[[1]],
+    cols = c(1:(nanc)), pos = pos
+)
+
+dev.off()
