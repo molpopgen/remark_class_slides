4e1ffad0c86c058a08da3188362f78e4f10d5aa9
molpopgen <krthornt@uci.edu>
2022-03-08 16:32:11 -0800

    auto recessive logic draft

--- genetics/figures/autosomal_recessive.R
+++ genetics/figures/autosomal_recessive.R
@@ -8,8 +8,36 @@ fam <- ped(
     sex = c(1, 2, 1, 2, 2, 2, 1, 1, 2, 2, 1)
 )
 
+affecteds <- c(4, 6)
+
 plot(fam,
-    symbolsize = 1.5, aff = c(4, 6),
+    symbolsize = 1.5, aff = affecteds,
+    hints = list(order = c(1:11), spouse = rbind(c(1, 2, 0), c(6, 7, 1)))
+)
+
+dev.off()
+
+# Add markers to the affecteds
+png("autosomal_recessive_affected_markers.png", pointsize = 18, width = 500)
+
+maff <- marker(fam, "4" = "a/a", "6" = "a/a")
+
+plot(fam,
+    marker = maff,
+    symbolsize = 1.5, aff = affecteds,
+    hints = list(order = c(1:11), spouse = rbind(c(1, 2, 0), c(6, 7, 1)))
+)
+
+dev.off()
+
+# Add markers to the parents
+png("autosomal_recessive_affected_parents_markers.png", pointsize = 18, width = 500)
+
+mpar <- marker(fam, "1" = "A/a", "2" = "A/a", "4" = "a/a", "6" = "a/a")
+
+plot(fam,
+    marker = mpar,
+    symbolsize = 1.5, aff = affecteds,
     hints = list(order = c(1:11), spouse = rbind(c(1, 2, 0), c(6, 7, 1)))
 )
 
