13f1aafb2c6e5417a321d456e5dd4a6a0a0df26a
molpopgen <krthornt@uci.edu>
2022-03-10 11:49:16 -0800

    works for 2 but not 1 trait :(

--- genetics/js/punnet_square.js
+++ genetics/js/punnet_square.js
@@ -1,12 +1,13 @@
 function punnet_square_table(parent1, parent2) {
     const cartesian =
         (...a) => a.reduce((a, b) => a.flatMap(d => b.map(e => [d, e].flat())));
+    const zip = (a, b) => a.map((k, i) => [k, b[i]].join(""));
 
-    let offspring_alleles = [];
+    let parent1_gametes = cartesian(...parent1);
+    let parent2_gametes = cartesian(...parent2);
 
-    const zip = (a, b) => a.map((k, i) => [k, b[i]]);
+    let offspring_genotypes = [];
 
-    let zipped = zip(parent1, parent2);
-
-    zipped.forEach((a) => offspring_alleles.push(cartesian(a[0], a[1])));
+    parent1_gametes.forEach((item, _) => { parent2_gametes.forEach((item2, _) => offspring_genotypes.push(zip(item, item2).join(""))) });
+    console.log(offspring_genotypes);
 }
