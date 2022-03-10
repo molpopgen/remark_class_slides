6d6f93abc55cc38c78edd3df5dab04c4708a0641
molpopgen <krthornt@uci.edu>
2022-03-10 12:34:01 -0800

    cleanup

--- genetics/js/punnet_square.js
+++ genetics/js/punnet_square.js
@@ -10,18 +10,18 @@ function _join(a, b) {
 function punnet_square_table(parent1, parent2) {
     const cartesian =
         (...a) => a.reduce((a, b) => a.flatMap(d => b.map(e => [d, e].flat())));
-    const zip = (a, b) => a.map((k, i) => [k, b[i]].join(""));
+    // const zip = (a, b) => a.map((k, i) => [k, b[i]].join(""));
 
     let parent1_gametes = cartesian(...parent1);
     let parent2_gametes = cartesian(...parent2);
 
-    console.log(parent1_gametes);
-    console.log(parent2_gametes);
+    // console.log(parent1_gametes);
+    // console.log(parent2_gametes);
 
     parent1_gametes.map((a, i) => console.log(a, i));
 
     let offspring_genotypes = [];
 
     parent1_gametes.forEach((item, _) => { parent2_gametes.forEach((item2, _) => offspring_genotypes.push(_join(item, item2))) });
-    console.log(offspring_genotypes);
+    // console.log(offspring_genotypes);
 }
